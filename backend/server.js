bbbb2de5eaa81fb3fdf1a61abbaaaac68ea99c8b
ABHILASH MELLACHERUVU <abhilashm9585@gmail.com>
2025-09-14 01:05:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: "https://lead-management-system-five-omega.vercel.app/",
     credentials: true,
   })
 );
