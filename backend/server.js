bfa9de7c6c8a1badf2af8908e431b5d668fac3cc
ABHILASH MELLACHERUVU <abhilashm9585@gmail.com>
2025-09-14 01:37:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://lead-management-system-lyart.vercel.app/,
+    origin: "https://lead-management-system-lyart.vercel.app/",
     credentials: true,
   })
 );
