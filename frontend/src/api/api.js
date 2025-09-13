77c843e97163348cb509e06f2c6a2ad751054611
ABHILASH MELLACHERUVU <abhilashm9585@gmail.com>
2025-09-14 01:23:48 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://lead-management-system-conk.onrender.com/", 
+  baseURL: "https://lead-management-system-conk.onrender.com/api", 
   withCredentials: true, 
 });
 
